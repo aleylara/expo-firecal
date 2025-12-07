3e1f46d121d98dbb16be3e60b5ba33440823bd9e
aleylara <ingirtere@gmail.com>
2025-12-07 19:46:40 +1100

    fix chevron color different opacity

--- src/components/bottomsheet/custom-bottom-sheet-handle.tsx
+++ src/components/bottomsheet/custom-bottom-sheet-handle.tsx
@@ -220,6 +220,7 @@ const styles = StyleSheet.create({
     width: 14,
     height: 5,
     marginTop: -2.5, // Center vertically
+    opacity: 0.7,
   },
   leftIndicator: {
     borderTopStartRadius: 2,
